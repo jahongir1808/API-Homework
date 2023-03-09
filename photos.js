297d8b1f1fbb5ee5e393a128ce2f377f7c3fe322
jahongir1808 <jahongir.developer18@gmail.com>
2023-03-10 01:20:11 +0500

    sdasd

--- photos.js
+++ photos.js
@@ -1,4 +1,5 @@
 const photosContainer = document.querySelector('.photos-cards')
+let pagination = document.querySelector('.pagination')
 
 function getCardPhotos({ title, url, thumbnailUrl }) {
 	return `
@@ -11,15 +12,16 @@ function getCardPhotos({ title, url, thumbnailUrl }) {
 }
 
 let page = 1
-let limit = 30
+let limit = 50
 
 async function getUser() {
 	let photosId = localStorage.getItem('usersId')
 	photosContainer.innerHTML = '...loading'
 	let res = await fetch(
-		`https://jsonplaceholder.typicode.com/users/${photosId}/photos?_page=${page}&_limit=${limit}`
-	)
-	let photos = await res.json()
+    `https://jsonplaceholder.typicode.com/users/${photosId}/photos?_page=${page}&_limit=${limit}`
+    )
+    let photos = await res.json()
+    console.log(photos);
 	photosContainer.innerHTML = ''
 	photos.forEach(element => {
 		photosContainer.innerHTML += getCardPhotos(element)
@@ -29,3 +31,44 @@ async function getUser() {
 }
 
 getUser()
+
+function getPagination() {
+	let pagination_numbers = ''
+	Array(10)
+		.fill(1)
+		.forEach((item, index) => {
+			pagination_numbers += `<li class="page-item ${
+				page == index + 1 ? 'active' : ''
+			}" onclick="getPage(${index + 1})">
+        <span class="page-link">
+          ${index + 1}
+        </span>
+      </li>`
+		})
+
+	pagination.innerHTML = `
+    <li onclick="getPage('-')" class="page-item ${
+			page == 1 ? 'disabled' : ''
+		}"><button class="page-link" href="#">Previous</button></li>
+    ${pagination_numbers}
+    <li onclick="getPage('+')" class="page-item ${
+			page == 20 ? 'disabled' : ''
+		}"><button class="page-link" href="#">Next</button></li>
+  `
+}
+
+getPagination()
+
+function getPage(p) {
+	if (p == '+') {
+		page++
+	} else if (p == '-') {
+		page--
+	} else {
+		page = p
+	}
+	if (page <= 10) {
+		getUser()
+		getPagination()
+	}
+}
